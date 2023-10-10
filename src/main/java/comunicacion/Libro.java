f05f32860854ce34dc2fdf388d1741a8fb70462c
Camila Rios <mriosm@unal.edu.co>
2023-10-10 07:35:42 -0500

    2

--- src/main/java/comunicacion/Libro.java
+++ src/main/java/comunicacion/Libro.java
@@ -1,5 +1,62 @@
 package comunicacion;
 
-public class Libro {
+public class Libro extends Escrito{
+	
+	private String co_autor;
+	private String editorial;
+	private String edicion;
+	private String interpretacion;
+	
+	public Libro(String origen, String title, String by, int page, String co, String editorial, String edicion, String interpretacion) {
+		super(origen, title, by, page);
+		this.co_autor = co;
+		this.editorial = editorial;
+		this.edicion = edicion;
+		this.interpretacion = interpretacion;
+	}
+	
+	public String getCo_autor() {
+		return co_autor;
+	}
+	public void setCo_autor(String co) {
+		this.co_autor = co;
+	}
+	public String getEditorial() {
+		return editorial;
+	}
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+	public String getEdicion() {
+		return edicion;
+	}
+	public void setEdicion(String edicion) {
+		this.edicion = edicion;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	public int palabrasTotales(int count) {
+		int cant = super.getPaginas() * count * 2;
+		return cant;
+	}
+	public String interpretacion() {
+		return interpretacion;
+	}
+	public String toString() {
+		String m = super.getOrigen() + "\n";
+		m += super.getTitulo() + "\n";
+		m += super.getAutor() + "\n";
+		m += super.getPaginas() + "\n";
+		m += this.co_autor + "\n";
+		m += this.editorial + "\n";
+		m += this.edicion;
+		return m;
+	}
+	
 
 }
